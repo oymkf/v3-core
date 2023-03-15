49268f3d0d70c0f695982f9981bd7a51acf3f0fe
mkfoyw <mkfoyw@gmail.com>
2023-03-15 10:31:01 +0800

    update code

---
+++ test/TestUtils.sol
@@ -0,0 +1,5 @@
+pragma solidity >=0.7.0;
+
+abstract contract TestUtils {
+    function sqrtP(uint256 price) internal pure returns (uint160) {}
+}
--- test/UniswapV3Pool.t.sol
+++ test/UniswapV3Pool.t.sol
@@ -5,6 +5,7 @@ import "forge-std/Test.sol";
 import "./ERC20Mintable.sol";
 import "../src/UniswapV3Factory.sol";
 import "../src/UniswapV3PoolDeployer.sol";
+import "../src/interfaces/IUniswapV3Pool.sol";
 
 contract UniswapV3PoolTest is Test {
     ERC20Mintable weth;
@@ -33,5 +34,50 @@ contract UniswapV3PoolTest is Test {
             address(usdt),
             500
         );
+
+        IUniswapV3Pool pool = IUniswapV3Pool(poolAddress);
+
+        assertEq(
+            factory.getPool(address(weth), address(usdt), 500),
+            poolAddress,
+            "invalid pool address in the registry"
+        );
+
+        assertEq(
+            factory.getPool(address(usdt), address(weth), 500),
+            poolAddress,
+            "invalid token0 address in the pool"
+        );
+
+        assertEq(pool.factory(), address(factory), "invalid factory address");
+        assertEq(pool.token0(), address(usdt), "invalid weth address");
+        assertEq(pool.token1(), address(weth), "invalid usdt address");
+
+        // pool.initialize(5000 << 96);
+        (
+            uint160 sqrtPriceX96,
+            int24 tick,
+            uint16 observationIndex,
+            uint16 observationCardinality,
+            uint16 observationCardinalityNext,
+            uint8 feeProtocol,
+            bool unlocked
+        ) = pool.slot0();
+
+        assertEq(uint256(sqrtPriceX96), uint256(0), "invalid sqrtPriceX96");
+        assertEq(int256(tick), 0, "invalid tick");
+        assertEq(uint256(observationIndex), 0, "invalid observationIndex");
+        assertEq(
+            uint256(observationCardinality),
+            0,
+            "invalid observationCardinality"
+        );
+        assertEq(
+            uint256(observationCardinalityNext),
+            0,
+            "invalid observationCardinalityNext"
+        );
+        assertEq(uint256(feeProtocol), 0, "invalid feeProtocol");
+        assertEq(unlocked, false, "invalid unlocked");
     }
 }
